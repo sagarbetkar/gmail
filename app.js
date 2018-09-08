79ba0bafbb86693330efe728c20c41c766d1eba0
Sagar Betkar <sagarbetkar1994@gmail.com>
2018-09-08 18:51:43 +0530

    Added users api

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+/**
+ * Controllers (route handlers).
+ */
+ const userController = require('./controllers/user');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,4 +23,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+mongoose.connect('mongodb://localhost:27017/gmail');
+mongoose.connection.on('error', (error) => console.error(error));
+mongoose.connection.on('open', () => console.log("success in connecting to mongodb"));
+
+app.post('/api/v1/users', userController.postNewUser);
+app.get('/api/v1/users', userController.getAllUsers);
+app.get('/api/v1/users/:id', userController.getUserById);
+app.put('/api/v1/users/:id', userController.updateUserById);
+app.delete('/api/v1/users/:id', userController.deleteUserById);
+
 module.exports = app;
+app.listen(2611, () => console.log('Express server at 2611'));
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  passwordExpiry: String,
+  createdAt: Date,
+  modifiedAt: Date,
+  firstName: String,
+  lastName: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
